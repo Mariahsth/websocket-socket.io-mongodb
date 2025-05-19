fd16170ad66b15e59e9dbbb81a4f210841058586
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-05-19 16:19:11 -0300

    Update autorizarUsuario.js

--- servidor/middlewares/autorizarUsuario.js
+++ servidor/middlewares/autorizarUsuario.js
@@ -2,8 +2,9 @@ import jwt from "jsonwebtoken";
 
 function autorizarUsuario(socket, next) {
     const tokenJwt= socket.handshake.auth.token;
+    const secret = process.env.JWT_SECRET || 'fallback-local-secret';
     try {
-        const payloadToken= jwt.verify(tokenJwt, process.env.SEGREDO_JWT);
+        const payloadToken= jwt.verify(tokenJwt, secret);
         socket.emit("autorizacao_sucesso", payloadToken)
         next();
     } catch (erro) {
