b3e60530e0a79e971cabb49b4f2a08966054d960
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-05-19 15:46:58 -0300

    Update socket-front-index.js

--- public/socket-front-index.js
+++ public/socket-front-index.js
@@ -1,6 +1,6 @@
 import { inserirLinkDocumento, removerLinkDocumento } from "./index.js";
 import { obterCookie } from "./utils/cookies.js";
-import io from "../../src/servidor.js";
+import io from "../servidor/servidor.js";
 
 const socket = io("/usuarios", {
   auth: {
